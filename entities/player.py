c21b1c9e396bdf6c2183cd5c93865886d9d2bb06
Erick Fernandes <80919235+erickofs@users.noreply.github.com>
2024-10-01 12:00:46 -0300

    Refactor player class to separate module, update methods and remove unused code

--- entities/player.py
+++ entities/player.py
@@ -1,8 +1,34 @@
 #player.py
 
 class Player:
-    def __init__(self, x, y): 
+    def __init__(self): 
         self.hp = 10
         self.weapon = 0
         self.armor = 0
+    
+    def move(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def defend(self):
+        pass
+
+    def take_damage(self):
+        pass
+    
+    def die(self):
+        pass
+
+    def equip_weapon(self):
+        pass
+
+    def equip_armor(self):
+        pass
+
+    def use_item(self):
+        pass
+
+    
 
